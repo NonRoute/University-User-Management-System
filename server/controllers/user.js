c697e4f7c29b81f3b64481acaa7462f60b26e848
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-05 00:23:45 +0700

    feat: login endpoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,10 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //@desc     GET all users
-//@route    GET /
+//@route    GET /user
 //@access   Public
 exports.getUsers = async (req, res, next) => {
   try {
@@ -15,7 +16,7 @@ exports.getUsers = async (req, res, next) => {
 }
 
 //@desc     Create user
-//@route    POST /
+//@route    POST /user
 //@access   Admin
 exports.createUser = async (req, res, next) => {
   const { username, password, role } = req.body
@@ -33,4 +34,60 @@ exports.createUser = async (req, res, next) => {
   } catch (err) {
     res.status(400).json({ message: err })
   }
+}
+
+//@desc		Login user
+//@route	POST /user/login
+//@access	Public
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+
+    //Validate email & password
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Please provide an username and password' })
+    }
+
+    //Check for user
+    const user = await prisma.user.findUnique({
+      where: {
+        username
+      }
+    })
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' })
+    }
+    //Check if password matches
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+
+    sendTokenResponse(user, 200, res)
+  } catch (err) {
+    res.status(400).json({ message: err })
+  }
+}
+
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  })
+
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token
+  })
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { getUsers, createUser } = require('../controllers/user')
+const { getUsers, createUser, login } = require('../controllers/user')
 const router = express.Router()
 
 router.route('/').get(getUsers).post(createUser)
+router.route('/login').post(login)
 
 module.exports = router
