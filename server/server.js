021f470a0482a30be0ea455d1287fe160b119d7e
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-05 18:15:15 +0700

    feat: setup swagger

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
 const user = require('./routes/user')
 const course = require('./routes/course')
@@ -12,6 +14,20 @@ app.use(morgan('dev'))
 
 app.use('/user', user)
 app.use('/course', course)
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Universiry User Management System API',
+      version: '1.0.0',
+      description: 'API Document'
+    }
+  },
+  apis: ['./routes/*.js']
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 const server = app.listen(8080, () =>
   console.log(`
