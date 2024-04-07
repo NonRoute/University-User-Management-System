97cc410cef7abaca7c0634a2c040a2c87fb2417f
NonRoute <chanon.ratn@gmail.com>
2024-04-07 16:43:05 +0700

    feat: remove Inter font

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'University user management system',
@@ -11,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <div className="bg-gradient-to-br from-cyan-500 to-sky-600 min-h-screen">{children}</div>
       </body>
     </html>
