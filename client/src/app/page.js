77127f3ead842aea94cb28c74bb27b712c151adf
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-07 23:56:27 +0700

    feat: add user info in navbar

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,18 +1,24 @@
+import Navbar from '@/components/Navbar'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="text-2xl md:text-3xl font-bold text-white text-center">Welcome to</div>
-      <div className="text-4xl md:text-5xl font-bold text-white text-center drop-shadow-md mt-2">
-        University user management system
+    <>
+      <Navbar />
+      <div className="absolute top-0 w-full">
+        <main className="flex flex-col items-center justify-center h-screen">
+          <div className="text-2xl md:text-3xl font-bold text-white text-center">Welcome to</div>
+          <div className="text-4xl md:text-5xl font-bold text-white text-center drop-shadow-md mt-2">
+            University user management system
+          </div>
+          <Link
+            href="/login"
+            className="text-center bg-white text-cyan-600 p-2 rounded-lg text-xl font-bold w-72 mt-16 drop-shadow-md hover:bg-gray-200 transition-all active:bg-gray-300 active:ring-4 ring-cyan-600"
+          >
+            Login
+          </Link>
+        </main>
       </div>
-      <Link
-        href="/login"
-        className="text-center bg-white text-cyan-600 p-2 rounded-lg text-xl font-bold w-72 mt-16 drop-shadow-md hover:bg-gray-200 transition-all active:bg-gray-300 active:ring-4 ring-cyan-600"
-      >
-        Login
-      </Link>
-    </main>
+    </>
   )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,19 @@
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Navbar() {
-  return <div className="bg-white h-14 text-black flex items-center justify-between p-4 shadow"></div>
+  const { data: session, status } = useSession()
+
+  return (
+    <div className="bg-white h-14 text-black flex items-center justify-between p-4 shadow">
+      <Link href={'/'}>
+        <Image src="logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+      <div className="flex gap-2 items-center">
+        <Image src={`${session.user.role}.svg`} alt="logo" width={30} height={30} />
+        <div className="font-semibold text-lg">{session.user.username}</div>
+      </div>
+    </div>
+  )
 }
