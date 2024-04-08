4744e37814dd970f95e2b8f4d5e25e031a4e3ed9
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-08 19:27:39 +0700

    fix: cannot read properties of null

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -33,7 +33,7 @@ exports.protect = async (req, res, next) => {
 //Grant access to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
+    if (!req?.user?.role || !roles.includes(req?.user?.role)) {
       return res.status(401).json({
         message: `User role ${req.user.role} is not authorized to access this route`
       })
