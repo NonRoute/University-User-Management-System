ee793d336469a459e8e60ac46b70a8cd7b32e8c5
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-09 13:43:47 +0700

    refactor: add space between line

--- client/src/components/AccessDenied.js
+++ client/src/components/AccessDenied.js
@@ -3,7 +3,9 @@ import { useEffect } from 'react'
 
 export default function AccessDenied() {
   const router = useRouter()
+
   useEffect(() => router.push('/login'), [])
+
   return (
     <div className="flex justify-center items-center h-screen text-white font-semibold text-4xl shadow-md">
       Access Denied
